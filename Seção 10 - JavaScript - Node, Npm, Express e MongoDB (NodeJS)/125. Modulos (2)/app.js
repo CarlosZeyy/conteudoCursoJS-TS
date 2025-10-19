70e128c6dda86a39ee92bccfd178a8aac378f7d3
Carlos Moises <carloszeeyy@gmail.com>
2025-10-19 17:51:47 -0300

    feat: enhance logging in app.js with additional directory and filename outputs

--- Seção 10 - JavaScript - Node, Npm, Express e MongoDB (NodeJS)/125. Modulos (2)/app.js
+++ Seção 10 - JavaScript - Node, Npm, Express e MongoDB (NodeJS)/125. Modulos (2)/app.js
@@ -4,9 +4,11 @@ const path = require("path");
 const p1 = new Person("Carlos");
 p1.saudacao();
 
+console.log('\nDirname: ')
 console.log(__dirname); //* Mostra o diretorio até a pasta
+console.log('\nFilname: ')
 console.log(__filename); //* Mostra o diretorio até o arquivo
 
-console.log()
+console.log(`\nPaths:`)
 console.log(path.resolve(__dirname, '..', '..', 'arquivos', 'qualquerCoisa'));
 console.log(path.resolve(__dirname, '.', '.', 'arquivos', 'qualquerCoisa'));

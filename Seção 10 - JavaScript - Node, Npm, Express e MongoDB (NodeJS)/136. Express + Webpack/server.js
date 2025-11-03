de261648f87954644a8c14870b7db7606dadfbf1
Carlos Moises <carloszeeyy@gmail.com>
2025-11-03 00:03:37 -0300

    feat: add Express server setup with routing and controllers
    - Created routes.js for defining application routes.
    - Implemented server.js to initialize Express app and middleware.
    - Added contactController.js and homeController.js for handling requests.
    - Developed index.ejs view for rendering the home page.
    - Configured webpack.config.js for bundling frontend assets.

---
+++ Seção 10 - JavaScript - Node, Npm, Express e MongoDB (NodeJS)/136. Express + Webpack/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { route } from "./routes.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const PORT = 5000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.resolve(__dirname, "public")));
+
+app.set("views", path.resolve(__dirname, "src", "views")); //* caminho absoluto
+// app.set("views", path.resolve('./src/views')); //* caminho relativo
+app.set("view engine", "ejs");
+
+app.use(route);
+
+app.listen(PORT, () => {
+  console.log(`Escutando o servidor na porta ${PORT}/`);
+  console.log(`Servidor on: http://localhost:${PORT}/`);
+});

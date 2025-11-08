8dc349727b532479ad512c433ec171b49505cfcf
Carlos Moises <carloszeeyy@gmail.com>
2025-11-08 10:56:15 -0300

    feat: add initial setup for Helmet and CSRF protection in Express app
    - Created routes for home and contact pages.
    - Implemented server configuration with Express, Mongoose, and session management.
    - Added controllers for handling home and contact page requests.
    - Introduced middleware for global variables and CSRF token handling.
    - Defined Mongoose model for Home with title and description fields.
    - Created views for 404 error, index page, and includes for head and footer.
    - Set up Webpack configuration for asset bundling.

---
+++ Seção 10 - JavaScript - Node, Npm, Express e MongoDB (NodeJS)/145. Helmet e CSRF/server.js
@@ -0,0 +1,65 @@
+import express from "express";
+import mongoose from "mongoose";
+import { configDotenv } from "dotenv";
+import { route } from "./routes.js";
+import path from "path";
+import helmet from 'helmet';
+import csurf from "csurf";
+import { fileURLToPath } from "url";
+import { middlewareGlobal, checkCsrfError, csrfMiddleware } from "./src/middlewares/middleware.js";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import flash from "connect-flash";
+
+const app = express();
+configDotenv();
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => {
+    app.emit("pronto");
+  })
+  .catch(e => console.log(e));
+
+const PORT = 5000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(helmet());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.resolve(__dirname, "public")));
+
+const sessionOptions = session({
+  secret: 'Testando session - posso colocar qualquer coisa',
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGODB,
+    // mongoOptions: { useNewUrlParser: true, useUnifiedTopology: true}
+  }),
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+    httpOnly: true 
+  }
+});
+
+app.use(sessionOptions);
+app.use(flash());
+
+app.set("views", path.resolve(__dirname, "src", "views")); //* caminho absoluto
+// app.set("views", path.resolve('./src/views')); //* caminho relativo
+app.set("view engine", "ejs");
+
+app.use(csurf());
+//* Middlewares
+app.use(middlewareGlobal);
+app.use(checkCsrfError);
+app.use(csrfMiddleware);
+app.use(route);
+
+app.on("pronto", () => {
+  app.listen(PORT, () => {
+    console.log(`Escutando o servidor na porta ${PORT}/`);
+    console.log(`Servidor on: http://localhost:${PORT}/`);
+  });
+});

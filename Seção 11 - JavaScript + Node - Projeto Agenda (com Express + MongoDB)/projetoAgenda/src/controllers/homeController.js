d0c3d5a1389f4c0f6eb009e9e9d0ec81566195ac
Carlos Moises <carloszeeyy@gmail.com>
2025-11-09 23:43:03 -0300

    refactor: simplify homePage rendering by removing unnecessary data

--- Seção 11 - JavaScript + Node - Projeto Agenda (com Express + MongoDB)/projetoAgenda/src/controllers/homeController.js
+++ Seção 11 - JavaScript + Node - Projeto Agenda (com Express + MongoDB)/projetoAgenda/src/controllers/homeController.js
@@ -1,8 +1,5 @@
 export function homePage(req, res) {
-  res.render(`index`, {
-    titulo: "Titulo da pagina",
-    numeros: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-  });
+  res.render(`index`);
   return;
 }
 

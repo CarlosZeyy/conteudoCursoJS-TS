76e4ff93037a6621ae2d25c7a9b0cced1277d64d
Carlos Moises <carloszeeyy@gmail.com>
2025-11-14 14:33:54 -0300

    style: format code for consistency in signupController.js

--- Seção 11 - JavaScript + Node - Projeto Agenda (com Express + MongoDB)/projetoAgenda/src/controllers/signupController.js
+++ Seção 11 - JavaScript + Node - Projeto Agenda (com Express + MongoDB)/projetoAgenda/src/controllers/signupController.js
@@ -4,9 +4,10 @@ export const signup = (req, res) => {
   res.render("signup");
 };
 
+
 export const register = async (req, res) => {
   const redirectBack = () => res.redirect(req.get("Referer") || "signup");
-
+  
   try {
     const signup = new SignUp(req.body);
     await signup.register();

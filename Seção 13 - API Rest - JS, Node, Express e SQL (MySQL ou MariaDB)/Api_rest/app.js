5cfd25ac9660d78676926c2e36f2c780b6a134ad
CarlosZeyy <carloszeeyy@gmail.com>
2026-01-02 20:48:25 -0300

    feat: implement initial server setup with routing and controller for home endpoint

---
+++ Seção 13 - API Rest - JS, Node, Express e SQL (MySQL ou MariaDB)/Api_rest/app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import HomeRoutes from './src/routes/homeRoutes.js';
+
+class App {
+  constructor() {
+    this.app = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(express.json());
+  }
+
+  routes() {
+    this.app.use('/', HomeRoutes);
+  }
+}
+
+
+export default new App().app;

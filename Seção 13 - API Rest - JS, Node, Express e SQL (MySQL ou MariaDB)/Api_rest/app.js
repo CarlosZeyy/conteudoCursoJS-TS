ad07164088f55dedc3598d3c8f4e65059e7fc9b4
CarlosZeyy <carloszeeyy@gmail.com>
2026-01-12 20:27:00 -0300

    feat: implement token generation and validation with JWT; add TokenController and routes

--- Seção 13 - API Rest - JS, Node, Express e SQL (MySQL ou MariaDB)/Api_rest/app.js
+++ Seção 13 - API Rest - JS, Node, Express e SQL (MySQL ou MariaDB)/Api_rest/app.js
@@ -3,6 +3,7 @@ dotenv.config();
 import express from 'express';
 import homeRoutes from './src/routes/homeRoutes.js';
 import userRoutes from './src/routes/userRoutes.js';
+import tokenRoutes from './src/routes/tokenRoutes.js';
 import './src/database';
 
 
@@ -21,6 +22,7 @@ class App {
   routes() {
     this.app.use('/', homeRoutes);
     this.app.use('/users/', userRoutes);
+    this.app.use('/tokens', tokenRoutes);
   }
 }
 

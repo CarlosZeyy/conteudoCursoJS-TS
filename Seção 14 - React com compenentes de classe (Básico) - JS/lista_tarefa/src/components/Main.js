5943598743badbb0d356309bb2e22a8574f1e6ca
CarlosZeyy <carloszeeyy@gmail.com>
2026-02-09 12:00:58 -0300

    feat: add react-icons dependency and enhance Main component with styled input and button

--- Seção 14 - React com compenentes de classe (Básico) - JS/lista_tarefa/src/components/Main.js
+++ Seção 14 - React com compenentes de classe (Básico) - JS/lista_tarefa/src/components/Main.js
@@ -1,28 +1,37 @@
-import React, {Component} from "react";
-import './Main.css';
+import React, { Component } from "react";
+import { FaPlus } from "react-icons/fa";
+import "./Main.css";
 
 export default class Main extends Component {
   state = {
-    novaTarefa: '',
-  }
+    novaTarefa: "",
+  };
 
   handlerChange = (e) => {
     this.setState({
       novaTarefa: e.target.value,
-    })
-  }
-
+    });
+  };
 
   render() {
+    const { novaTarefa } = this.state;
+
     return (
       <div className="main">
         <h1>Lista de tarefas</h1>
 
-        <form action="#">
-          <input onChange={this.handlerChange} type="text"/>
-          <button type="submit">Enviar</button>
+        <form action="#" className="form">
+          <input
+            onChange={this.handlerChange}
+            type="text"
+            className="inputField"
+            value={novaTarefa}
+          />
+          <button type="submit" className="sendBtn">
+            <FaPlus />
+          </button>
         </form>
       </div>
-    )
+    );
   }
 }
